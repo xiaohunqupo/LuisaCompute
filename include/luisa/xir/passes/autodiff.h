cf019a61b28b6e6decab174315a54dc46c1ce56b
Xiaochun Tong <xtong@uwaterloo.ca>
2025-01-31 03:23:02 -0500

    added some autodiff stuff

---
+++ include/luisa/xir/passes/autodiff.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <luisa/core/dll_export.h>
+#include <luisa/core/stl/unordered_map.h>
+
+namespace luisa::compute::xir {
+
+class Module;
+class Function;
+struct AutodiffInfo {
+};
+struct AutodiffOptions {
+    bool run_forwad{true};
+    bool run_backward{true};
+};
+LC_XIR_API void autodiff_pass_run_on_function(Function *function, const AutodiffOptions &options = {}) noexcept;
+LC_XIR_API void autodiff_pass_run_on_module(Module *module, const AutodiffOptions &options = {}) noexcept;
+
+}// namespace luisa::compute::xir
