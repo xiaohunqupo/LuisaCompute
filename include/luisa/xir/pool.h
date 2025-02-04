0cdd7f7b59f9cf27262319b5826100169968c102
Mike Smith <mango@live.cn>
2025-02-04 17:17:26 +0800

    better isa implementation

--- include/luisa/xir/pool.h
+++ include/luisa/xir/pool.h
@@ -8,21 +8,18 @@
 
 namespace luisa::compute::xir {
 
-#define LUISA_XIR_DEFINED_ISA_METHOD(BaseType, base_name)                                            \
-    template<typename Derived>                                                                       \
-        requires std::derived_from<Derived, BaseType>                                                \
-    [[nodiscard]] bool isa() const noexcept {                                                        \
-        if constexpr (std::is_same_v<BaseType, Derived>) {                                           \
-            return true;                                                                             \
-        } else {                                                                                     \
-            using ImmediateDerived = typename Derived::derived_##base_name##_type;                   \
-            if constexpr (std::is_final_v<ImmediateDerived>) {                                       \
-                return derived_##base_name##_tag() == Derived::static_derived_##base_name##_tag();   \
-            } else {                                                                                 \
-                return derived_##base_name##_tag() == Derived::static_derived_##base_name##_tag() && \
-                       static_cast<const ImmediateDerived *>(this)->template isa<Derived>();         \
-            }                                                                                        \
-        }                                                                                            \
+#define LUISA_XIR_DEFINED_ISA_METHOD(BaseType, base_name)                                             \
+    template<typename Derived>                                                                        \
+        requires std::derived_from<Derived, BaseType>                                                 \
+    [[nodiscard]] bool isa() const noexcept {                                                         \
+        if constexpr (std::is_same_v<BaseType, Derived>) {                                            \
+            return true;                                                                              \
+        } else {                                                                                      \
+            using ImmediateDerived = typename Derived::derived_##base_name##_type;                    \
+            return derived_##base_name##_tag() == Derived::static_derived_##base_name##_tag() &&      \
+                   (std::is_final_v<ImmediateDerived> || std::is_same_v<ImmediateDerived, Derived> || \
+                    static_cast<const ImmediateDerived *>(this)->template isa<Derived>());            \
+        }                                                                                             \
     }
 
 class Pool;
