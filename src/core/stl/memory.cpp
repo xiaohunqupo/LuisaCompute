e7beaf46e0ded14731d979031233c8430a1aed07
Mike <mango@live.cn>
2025-03-17 21:09:52 +0800

    fix realloc on windows

--- src/core/stl/memory.cpp
+++ src/core/stl/memory.cpp
@@ -38,12 +38,18 @@ LUISA_EXPORT_API void allocator_deallocate(void *p, size_t) noexcept {
 
 LUISA_EXPORT_API void *allocator_reallocate(void *p, size_t size, size_t alignment) noexcept {
 #ifdef LUISA_USE_SYSTEM_STL
+#ifdef _WIN32
+    alignment = std::max<size_t>(alignment, 16u);
+    size = luisa::align(size, alignment);
+    return _aligned_realloc(p, size, alignment);
+#else
     if (alignment != 0u && alignment <= alignof(std::max_align_t)) {
         return ::realloc(p, size);
     }
     auto new_p = allocator_allocate(size, alignment);
     std::memcpy(new_p, p, size);
     allocator_deallocate(p, 0u);
+#endif
     return new_p;
 #else
     p = eastl::GetDefaultAllocator()->reallocate(p, size);
