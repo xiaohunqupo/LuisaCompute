cdae076b853793e5689839405b39bf1a0f2b476f
Mike <mango@live.cn>
2025-04-05 17:53:24 +0800

    add lexical scope break analysis for cuda xir-codegen

--- src/xir/passes/reg2mem.cpp
+++ src/xir/passes/reg2mem.cpp
@@ -11,32 +11,35 @@ namespace luisa::compute::xir {
 
 namespace detail {
 
-static void lower_phi_nodes_in_function(Function *function, Reg2MemInfo &info) noexcept {
-    if (auto def = function->definition()) {
-        auto prev_size = info.lowered_phi_nodes.size();
-        def->traverse_instructions([&](Instruction *inst) noexcept {
-            if (inst->isa<PhiInst>()) {
-                info.lowered_phi_nodes.emplace_back(static_cast<PhiInst *>(inst));
-            }
-        });
-        for (auto phi : luisa::span{info.lowered_phi_nodes}.subspan(prev_size)) {
-            lower_phi_node_to_local_variable(phi);
+static void lower_phi_nodes_in_function(FunctionDefinition *def, Reg2MemInfo &info) noexcept {
+    auto prev_size = info.lowered_phi_nodes.size();
+    def->traverse_instructions([&](Instruction *inst) noexcept {
+        if (inst->isa<PhiInst>()) {
+            info.lowered_phi_nodes.emplace_back(static_cast<PhiInst *>(inst));
         }
+    });
+    for (auto phi : luisa::span{info.lowered_phi_nodes}.subspan(prev_size)) {
+        lower_phi_node_to_local_variable(phi);
     }
 }
 
-static void hoist_allocas_to_top_of_funtion(Function *function) noexcept {
-    if (auto def = function->definition()) {
-        luisa::vector<AllocaInst *> allocas;
-        def->traverse_instructions([&](Instruction *inst) noexcept {
-            if (inst->isa<AllocaInst>()) {
-                allocas.emplace_back(static_cast<AllocaInst *>(inst));
-            }
-        });
-        for (auto a : allocas) { a->remove_self(); }
-        XIRBuilder b;
-        b.set_insertion_point(def->body_block()->instructions().head_sentinel());
-        for (auto a : allocas) { b.append(a); }
+static void hoist_allocas_to_top_of_funtion(FunctionDefinition *def) noexcept {
+    luisa::vector<AllocaInst *> allocas;
+    def->traverse_instructions([&](Instruction *inst) noexcept {
+        if (inst->isa<AllocaInst>()) {
+            allocas.emplace_back(static_cast<AllocaInst *>(inst));
+        }
+    });
+    for (auto a : allocas) { a->remove_self(); }
+    XIRBuilder b;
+    b.set_insertion_point(def->body_block()->instructions().head_sentinel());
+    for (auto a : allocas) { b.append(a); }
+}
+
+static void run_reg2mem_pass_on_function(Function *function, Reg2MemInfo &info) noexcept {
+    if (auto definition = function->definition()) {
+        lower_phi_nodes_in_function(definition, info);
+        hoist_allocas_to_top_of_funtion(definition);
     }
 }
 
@@ -44,16 +47,14 @@ static void hoist_allocas_to_top_of_funtion(Function *function) noexcept {
 
 Reg2MemInfo reg2mem_pass_run_on_function(Function *function) noexcept {
     Reg2MemInfo info;
-    detail::lower_phi_nodes_in_function(function, info);
-    detail::hoist_allocas_to_top_of_funtion(function);
+    detail::run_reg2mem_pass_on_function(function, info);
     return info;
 }
 
 Reg2MemInfo reg2mem_pass_run_on_module(Module *module) noexcept {
     Reg2MemInfo info;
     for (auto &&f : module->function_list()) {
-        detail::lower_phi_nodes_in_function(&f, info);
-        detail::hoist_allocas_to_top_of_funtion(&f);
+        detail::run_reg2mem_pass_on_function(&f, info);
     }
     return info;
 }
