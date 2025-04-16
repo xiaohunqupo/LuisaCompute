dfe826ad35009737bbdd6a6082dd95251c7640de
Mike <mango@live.cn>
2025-04-16 22:10:15 +0800

    fix mem2reg

--- src/xir/passes/mem2reg.cpp
+++ src/xir/passes/mem2reg.cpp
@@ -17,13 +17,10 @@ namespace detail {
     if (inst->op() != AllocaOp::LOCAL) { return false; }
     // check if it's used as reference in other instructions than load/store
     for (auto &&use : inst->use_list()) {
-        if (auto user = use.user()) {
-            LUISA_DEBUG_ASSERT(user->isa<Instruction>(), "Invalid user.");
-            switch (auto user_inst = static_cast<Instruction *>(user); user_inst->derived_instruction_tag()) {
-                case DerivedInstructionTag::LOAD: break;
-                case DerivedInstructionTag::STORE: break;
-                default: return false;
-            }
+        LUISA_DEBUG_ASSERT(use.user() != nullptr && use.user()->isa<Instruction>(), "Invalid user.");
+        if (auto user_inst = static_cast<Instruction *>(use.user());
+            !user_inst->isa<LoadInst>() && !user_inst->isa<StoreInst>()) {
+            return false;
         }
     }
     return true;
@@ -32,8 +29,8 @@ namespace detail {
 struct AllocaAnalysis {
 
     const DomTree &dom;
-    const luisa::unordered_map<Instruction *, uint> inst_indices;
-    const luisa::unordered_map<BasicBlock *, uint> block_indices;
+    const luisa::unordered_map<Instruction *, uint> &inst_indices;
+    const luisa::unordered_map<BasicBlock *, uint> &block_indices;
 
     luisa::unordered_map<BasicBlock *, StoreInst *> def_blocks;
     luisa::unordered_map<BasicBlock *, LoadInst *> use_blocks;
@@ -67,7 +64,13 @@ struct AllocaAnalysis {
         live_in_blocks.clear();
         luisa::fixed_vector<BasicBlock *, 64u> work_list;
         work_list.reserve(use_blocks.size());
-        for (auto [use_block, _] : use_blocks) { work_list.emplace_back(use_block); }
+        for (auto [use_block, load] : use_blocks) {
+            if (auto def_iter = def_blocks.find(use_block); def_iter != def_blocks.end()) {
+                // make sure the store is after the load
+                LUISA_ASSERT(inst_indices.at(def_iter->second) > inst_indices.at(load), "Invalid state.");
+            }
+            work_list.emplace_back(use_block);
+        }
         // extend the live-in block set by adding all non-defining predecessors of the known live-in blocks
         while (!work_list.empty()) {
             auto block = work_list.back();
@@ -100,46 +103,31 @@ static void remove_alloca(AllocaInst *alloca_inst, Mem2RegInfo &info) noexcept {
 }
 
 struct PhiInsertionAndRenaming {
+
     luisa::unordered_map<BasicBlock *, PhiInst *> block_to_phi;
-    luisa::unordered_map<BasicBlock *, Value *> out_values;
 
     // the following fields are used across the processing of different alloca's
     luisa::vector<PhiInst *> inserted;
 
-    template<typename T>
     [[nodiscard]] Value *find_dom_value_for_use_block(BasicBlock *use_block, const Type *type,
-                                                      const luisa::unordered_map<BasicBlock *, T *> &defs_in_use_block,
-                                                      const AllocaAnalysis &analysis, bool consider_stores) noexcept {
-        // the load has a phi node in the same block
-        if (auto iter = defs_in_use_block.find(use_block); iter != defs_in_use_block.end()) {
-            return iter->second;
-        }
-        // the load is dominated by a store or a phi node
-        if (auto node = analysis.dom.node_or_null(use_block)) {
-            while (node != analysis.dom.root()) {
-                auto parent = node->parent();
-                LUISA_DEBUG_ASSERT(parent != nullptr, "Invalid parent.");
-                // store must have higher priority than phi nodes as it's closer to the use block
-                if (consider_stores) {
-                    if (auto iter = analysis.def_blocks.find(parent->block()); iter != analysis.def_blocks.end()) {
-                        return iter->second->value();
-                    }
-                }
-                // check phi nodes if no store is found
-                if (auto iter = out_values.find(parent->block()); iter != out_values.end()) {
-                    return iter->second;
-                }
-                node = parent;
+                                                      const AllocaAnalysis &analysis) noexcept {
+        for (auto node = analysis.dom.node_or_null(use_block); node != nullptr; node = node->parent()) {
+            // store must have higher priority than phi nodes as it's closer to the use block
+            if (auto iter = analysis.def_blocks.find(node->block()); iter != analysis.def_blocks.end()) {
+                return iter->second->value();
+            }
+            // check phi nodes if no store is found
+            if (auto iter = block_to_phi.find(node->block()); iter != block_to_phi.end()) {
+                return iter->second;
             }
         }
-        // not found, get an undef value
+        // if no dominant value found, get an undef value
         return use_block->parent_module()->create_undefined(type);
     }
 
     void place_phi_nodes(AllocaInst *inst, const AllocaAnalysis &analysis, Mem2RegInfo &info) noexcept {
         // insert new phi nodes by traversing the closure of dominance frontiers of the def blocks
         block_to_phi.clear();
-        out_values.clear();
         auto type = inst->type();
         luisa::fixed_vector<BasicBlock *, 64u> work_list;
         work_list.reserve(analysis.def_blocks.size());
@@ -157,12 +145,6 @@ struct PhiInsertionAndRenaming {
                         iter->second = phi;
                         inserted.emplace_back(phi);
                         info.inserted_phi_instructions.emplace(phi);
-                        // update the block-out phi value
-                        out_values[fb] = phi;
-                        // replace the load instructions in the same block with the new phi node
-                        if (auto use_iter = analysis.use_blocks.find(fb); use_iter != analysis.use_blocks.end()) {
-                            replace_load_with_value(use_iter->second, phi, info);
-                        }
                         // add the block to the work list to compute the closure
                         work_list.emplace_back(fb);
                     }
@@ -172,29 +154,41 @@ struct PhiInsertionAndRenaming {
         // each of the use blocks must be dominated by some def/phi block, or it must contain undefined value
         for (auto [use_block, load_inst] : analysis.use_blocks) {
             if (!info.removed_load_instructions.contains(load_inst)) {
-                auto dom_value = find_dom_value_for_use_block(use_block, type, block_to_phi, analysis, true);
-                replace_load_with_value(load_inst, dom_value, info);
+                if (auto phi_iter = block_to_phi.find(use_block); phi_iter != block_to_phi.end()) {
+                    // we have a phi node in the same block so we can replace the load with it
+                    replace_load_with_value(load_inst, phi_iter->second, info);
+                } else if (auto node = analysis.dom.node_or_null(use_block); node != nullptr && node != analysis.dom.root()) {
+                    // otherwise we walk the dom tree to find the value that dominates the use block
+                    auto dom_value = find_dom_value_for_use_block(node->parent()->block(), type, analysis);
+                    replace_load_with_value(load_inst, dom_value, info);
+                } else {
+                    auto undef = use_block->parent_module()->create_undefined(type);
+                    replace_load_with_value(load_inst, undef, info);
+                }
             }
         }
-        // overwrite the block-out values with the store values
-        for (auto [def_block, store_inst] : analysis.def_blocks) {
-            out_values[def_block] = store_inst->value();
+        // now the alloca should have no load uses but only store uses, check it
+        for (auto &&use : inst->use_list()) {
+            if (auto user = use.user()) {
+                LUISA_ASSERT(user->isa<StoreInst>(), "Invalid user.");
+            }
         }
-        // fill incomings of the phi nodes
+        // now we fill the incoming values of the phi nodes
         for (auto mapping : block_to_phi) {
             // earlier clang compilers have trouble with structural binding in lambda capture, so we manually unpack here
             auto phi_block = mapping.first;
             auto phi_inst = mapping.second;
             phi_block->traverse_predecessors(false, [&](BasicBlock *pred) noexcept {
-                auto dom_value = find_dom_value_for_use_block(pred, type, out_values, analysis, false);
+                auto dom_value = find_dom_value_for_use_block(pred, type, analysis);
                 phi_inst->add_incoming(dom_value, pred);
             });
         }
-        // remove the stores and update the block-out values, possibly overwriting previously recorded phi nodes
+        // remove the stores
         for (auto [def_block, store_inst] : analysis.def_blocks) {
             remove_store(store_inst, info);
         }
-        // remove the local variable and record the promotion
+        // remove the local variable (which should have no uses now) and record the promotion
+        LUISA_ASSERT(inst->use_list().empty(), "Invalid state.");
         remove_alloca(inst, info);
     }
 
--- src/xir/tests/test_mem2reg.cpp
+++ src/xir/tests/test_mem2reg.cpp
@@ -35,4 +35,76 @@ int main(int argc, char *argv[]) {
            << synchronize();
 
     LUISA_INFO("result = {}", result);
+
+    auto trace = [&](AccelVar &accel, const UInt &mask, const UInt &ignore) noexcept {
+        Var<CommittedHit> hit;
+        $if (mask >= 1u) {
+            Var<SurfaceHit> h;
+            h.inst = ~0u;
+            auto inside = mask > 100u;
+            $if (!inside) {
+                h = accel.intersect(make_ray(make_float3(), make_float3(1.f)), {});
+                hit.inst = h.inst;
+                hit.prim = h.prim;
+                hit.committed_ray_t = h.committed_ray_t;
+                hit.bary = h.bary;
+                hit.hit_type = ite(h->miss(), static_cast<uint32_t>(HitType::Miss), static_cast<uint32_t>(HitType::Surface));
+                $if (h->miss()) {
+                    inside = true;
+                };
+            };
+            $if (hit->miss() & inside) {
+                $for (i, 100u) {
+                    h = accel.intersect(make_ray(make_float3(), make_float3(1.f)), {});
+                    $if (hit->miss()) {
+                        $break;
+                    };
+                };
+            };
+            $if (h->miss()) {
+                h = accel.intersect(make_ray(make_float3(), make_float3(1.f)), {});
+            };
+            hit.inst = h.inst;
+            hit.prim = h.prim;
+            hit.committed_ray_t = h.committed_ray_t;
+            hit.bary = h.bary;
+            hit.hit_type = ite(h->miss(), static_cast<uint32_t>(HitType::Miss), static_cast<uint32_t>(HitType::Surface));
+        } $else {
+        };
+        return hit;
+    };
+
+    auto shader2 = device.compile<1>([&](AccelVar accel, BufferVar<CommittedHit> hits, UInt mask) noexcept {
+        Var<CommittedHit> hit;
+        hit->inst = ~0u;
+        $while (true) {
+            $if (mask >= 1u) {
+                auto h = accel.intersect(make_ray(make_float3(), make_float3(1.f)), {});
+                hit.inst = h.inst;
+                hit.prim = h.prim;
+                hit.committed_ray_t = h.committed_ray_t;
+                hit.bary = h.bary;
+                hit.hit_type = ite(h->miss(), static_cast<uint32_t>(HitType::Miss), static_cast<uint32_t>(HitType::Surface));
+                $if (hit->miss()) {
+                    $break;
+                };
+            } $else {
+                hit = trace(accel, mask, hit.prim);
+                $if (mask == 2u) {
+                    $if (hits->read(0u)->miss()) {
+                        $break;
+                    };
+                    $continue;
+                };
+            };
+            $if (!hit->miss()) {
+                hits->write(0u, hit);
+            };
+        };
+        // $if (mask != 0u) {
+        //     $if (!hit->miss()) {
+        //         hits->write(0u, hit);
+        //     };
+        // };
+    });
 }
