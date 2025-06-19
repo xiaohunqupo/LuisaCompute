52449a11802b9c219302b292c882901402d6b9ff
Yuefeng Geng <maxwellgeng@outlook.com>
2025-06-19 09:37:16 +0800

    Fix windows path with non-ascii char

--- src/ext/liblmdb/mdb.c
+++ src/ext/liblmdb/mdb.c
@@ -11428,13 +11428,72 @@ mdb_mutex_failed(MDB_env *env, mdb_mutexref_t mutex, int rc)
 
 #if defined(_WIN32)
 /** Convert \b src to new wchar_t[] string with room for \b xtra extra chars */
+static BOOL is_valid_utf8(const char * string)
+{
+    if (!string)
+        return 1;
+
+    const unsigned char * bytes = (const unsigned char *)string;
+    unsigned int cp;
+    int num;
+
+    while (*bytes != 0x00)
+    {
+        if ((*bytes & 0x80) == 0x00)
+        {
+            // U+0000 to U+007F 
+            cp = (*bytes & 0x7F);
+            num = 1;
+        }
+        else if ((*bytes & 0xE0) == 0xC0)
+        {
+            // U+0080 to U+07FF 
+            cp = (*bytes & 0x1F);
+            num = 2;
+        }
+        else if ((*bytes & 0xF0) == 0xE0)
+        {
+            // U+0800 to U+FFFF 
+            cp = (*bytes & 0x0F);
+            num = 3;
+        }
+        else if ((*bytes & 0xF8) == 0xF0)
+        {
+            // U+10000 to U+10FFFF 
+            cp = (*bytes & 0x07);
+            num = 4;
+        }
+        else
+            return 0;
+
+        bytes += 1;
+        for (int i = 1; i < num; ++i)
+        {
+            if ((*bytes & 0xC0) != 0x80)
+                return 0;
+            cp = (cp << 6) | (*bytes & 0x3F);
+            bytes += 1;
+        }
+
+        if ((cp > 0x10FFFF) ||
+            ((cp >= 0xD800) && (cp <= 0xDFFF)) ||
+            ((cp <= 0x007F) && (num != 1)) ||
+            ((cp >= 0x0080) && (cp <= 0x07FF) && (num != 2)) ||
+            ((cp >= 0x0800) && (cp <= 0xFFFF) && (num != 3)) ||
+            ((cp >= 0x10000) && (cp <= 0x1FFFFF) && (num != 4)))
+            return 0;
+    }
+
+    return 1;
+}
 static int ESECT
 utf8_to_utf16(const char *src, MDB_name *dst, int xtra)
 {
 	int rc, need = 0;
 	wchar_t *result = NULL;
+	int code_page = is_valid_utf8(src) ? CP_UTF8 : CP_ACP;
 	for (;;) {					/* malloc result, then fill it in */
-		need = MultiByteToWideChar(CP_UTF8, 0, src, -1, result, need);
+		need = MultiByteToWideChar(code_page, 0, src, -1, result, need);
 		if (!need) {
 			rc = ErrCode();
 			free(result);
