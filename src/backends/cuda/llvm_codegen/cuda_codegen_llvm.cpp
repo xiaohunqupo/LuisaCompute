5659d4990ceebb15256d922a9c116d0cd6fc6547
Mike Smith <mango@live.cn>
2025-09-17 22:11:19 +0800

    fix llvm linking

---
+++ src/backends/cuda/llvm_codegen/cuda_codegen_llvm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mike on 9/17/25.
+//
+
+#include "cuda_codegen_llvm.h"
---
+++ src/backends/cuda/llvm_codegen/cuda_codegen_llvm.h
@@ -0,0 +1,11 @@
+//
+// Created by mike on 9/17/25.
+//
+
+#ifndef LUISACOMPUTE_CUDA_CODEGEN_LLVM_H
+#define LUISACOMPUTE_CUDA_CODEGEN_LLVM_H
+
+class cuda_codegen_llvm {
+};
+
+#endif//LUISACOMPUTE_CUDA_CODEGEN_LLVM_H
