cdafbb12db6d103a1100b5d26c58970d28680069
maxwellgeng <maxwellgeng@outlook.com>
2025-09-22 17:37:34 +0800

    Preparing for auto-codegen

--- src/backends/cuda/cuda_builtin_embedded.h
+++ src/backends/cuda/cuda_builtin_embedded.h
@@ -1,7 +1,7 @@
 #pragma once
 
-extern "C" const char luisa_cuda_builtin_cuda_builtin_kernels[6094];
-extern "C" const char luisa_cuda_builtin_cuda_device_coop[76548];
-extern "C" const char luisa_cuda_builtin_cuda_device_half[81134];
-extern "C" const char luisa_cuda_builtin_cuda_device_math[579196];
-extern "C" const char luisa_cuda_builtin_cuda_device_resource[118114];
+extern "C" const unsigned char cuda_builtin_kernels[6094];
+extern "C" const unsigned char cuda_device_coop[76548];
+extern "C" const unsigned char cuda_device_half[81134];
+extern "C" const unsigned char cuda_device_math[579196];
+extern "C" const unsigned char cuda_device_resource[118114];
