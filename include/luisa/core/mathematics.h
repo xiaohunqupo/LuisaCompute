bb876ecc84d85126c8c2d25a6a0b3d5f806bbac2
Yuefeng Geng <maxwellgeng@outlook.com>
2025-11-17 02:57:22 +0800

    double mathematics

--- include/luisa/core/mathematics.h
+++ include/luisa/core/mathematics.h
@@ -76,7 +76,10 @@ using std::min;
 #define LUISA_MAKE_VECTOR_UNARY_FUNC(func)                                                                \
     [[nodiscard]] inline auto func(float2 v) noexcept { return float2{func(v.x), func(v.y)}; }            \
     [[nodiscard]] inline auto func(float3 v) noexcept { return float3{func(v.x), func(v.y), func(v.z)}; } \
-    [[nodiscard]] inline auto func(float4 v) noexcept { return float4{func(v.x), func(v.y), func(v.z), func(v.w)}; }
+    [[nodiscard]] inline auto func(float4 v) noexcept { return float4{func(v.x), func(v.y), func(v.z), func(v.w)}; }\
+    [[nodiscard]] inline auto func(double2 v) noexcept { return double2{func(v.x), func(v.y)}; }            \
+    [[nodiscard]] inline auto func(double3 v) noexcept { return double3{func(v.x), func(v.y), func(v.z)}; } \
+    [[nodiscard]] inline auto func(double4 v) noexcept { return double4{func(v.x), func(v.y), func(v.z), func(v.w)}; }
 
 LUISA_MAKE_VECTOR_UNARY_FUNC(acos)
 LUISA_MAKE_VECTOR_UNARY_FUNC(asin)
@@ -131,22 +134,30 @@ LUISA_MAKE_VECTOR_BINARY_FUNC(fmod)
 #undef LUISA_MAKE_VECTOR_BINARY_FUNC
 
 [[nodiscard]] inline auto isnan(float x) noexcept {
-    auto u = 0u;
-    std::memcpy(&u, &x, sizeof(float));
-    return (u & 0x7f800000u) == 0x7f800000u && (u & 0x007fffffu) != 0u;
+    return std::isnan(x);
 }
 [[nodiscard]] inline auto isnan(float2 v) noexcept { return make_bool2(isnan(v.x), isnan(v.y)); }
 [[nodiscard]] inline auto isnan(float3 v) noexcept { return make_bool3(isnan(v.x), isnan(v.y), isnan(v.z)); }
 [[nodiscard]] inline auto isnan(float4 v) noexcept { return make_bool4(isnan(v.x), isnan(v.y), isnan(v.z), isnan(v.w)); }
+[[nodiscard]] inline auto isnan(double x) noexcept {
+    return std::isnan(x);
+}
+[[nodiscard]] inline auto isnan(double2 v) noexcept { return make_bool2(isnan(v.x), isnan(v.y)); }
+[[nodiscard]] inline auto isnan(double3 v) noexcept { return make_bool3(isnan(v.x), isnan(v.y), isnan(v.z)); }
+[[nodiscard]] inline auto isnan(double4 v) noexcept { return make_bool4(isnan(v.x), isnan(v.y), isnan(v.z), isnan(v.w)); }
 
 [[nodiscard]] inline auto isinf(float x) noexcept {
-    auto u = 0u;
-    std::memcpy(&u, &x, sizeof(float));
-    return (u & 0x7f800000u) == 0x7f800000u && (u & 0x007fffffu) == 0u;
+    return std::isinf(x);
 }
 [[nodiscard]] inline auto isinf(float2 v) noexcept { return make_bool2(isinf(v.x), isinf(v.y)); }
 [[nodiscard]] inline auto isinf(float3 v) noexcept { return make_bool3(isinf(v.x), isinf(v.y), isinf(v.z)); }
 [[nodiscard]] inline auto isinf(float4 v) noexcept { return make_bool4(isinf(v.x), isinf(v.y), isinf(v.z), isinf(v.w)); }
+[[nodiscard]] inline auto isinf(double x) noexcept {
+    return std::isinf(x);
+}
+[[nodiscard]] inline auto isinf(double2 v) noexcept { return make_bool2(isinf(v.x), isinf(v.y)); }
+[[nodiscard]] inline auto isinf(double3 v) noexcept { return make_bool3(isinf(v.x), isinf(v.y), isinf(v.z)); }
+[[nodiscard]] inline auto isinf(double4 v) noexcept { return make_bool4(isinf(v.x), isinf(v.y), isinf(v.z), isinf(v.w)); }
 
 // min
 template<typename T, std::enable_if_t<is_scalar_v<T>, int> = 0>
@@ -315,6 +326,38 @@ template<size_t N>
                   u.x * v.y - v.x * u.y};
 }
 
+[[nodiscard]] constexpr auto dot(double2 u, double2 v) noexcept {
+    return u.x * v.x + u.y * v.y;
+}
+[[nodiscard]] constexpr auto dot(double3 u, double3 v) noexcept {
+    return u.x * v.x + u.y * v.y + u.z * v.z;
+}
+[[nodiscard]] constexpr auto dot(double4 u, double4 v) noexcept {
+    return u.x * v.x + u.y * v.y + u.z * v.z + u.w * v.w;
+}
+
+template<size_t N>
+[[nodiscard]] constexpr auto length(Vector<double, N> u) noexcept {
+    return sqrt(dot(u, u));
+}
+
+template<size_t N>
+[[nodiscard]] constexpr auto normalize(Vector<double, N> u) noexcept {
+    return u * (1.0f / length(u));
+}
+
+template<size_t N>
+[[nodiscard]] constexpr auto distance(Vector<double, N> u, Vector<double, N> v) noexcept {
+    return length(u - v);
+}
+
+[[nodiscard]] constexpr auto cross(double3 u, double3 v) noexcept {
+    return double3{u.y * v.z - v.y * u.z,
+                  u.z * v.x - v.z * u.x,
+                  u.x * v.y - v.x * u.y};
+}
+
+
 // Matrix Functions
 [[nodiscard]] constexpr auto transpose(const float2x2 m) noexcept {
     return make_float2x2(m[0].x, m[1].x,
@@ -329,6 +372,24 @@ template<size_t N>
 
 [[nodiscard]] constexpr auto transpose(const float4x4 m) noexcept {
     return make_float4x4(m[0].x, m[1].x, m[2].x, m[3].x,
+                         m[0].y, m[1].y, m[2].y, m[3].y,
+                         m[0].z, m[1].z, m[2].z, m[3].z,
+                         m[0].w, m[1].w, m[2].w, m[3].w);
+}
+
+[[nodiscard]] constexpr auto transpose(const double2x2 m) noexcept {
+    return make_double2x2(m[0].x, m[1].x,
+                         m[0].y, m[1].y);
+}
+
+[[nodiscard]] constexpr auto transpose(const double3x3 m) noexcept {
+    return make_double3x3(m[0].x, m[1].x, m[2].x,
+                         m[0].y, m[1].y, m[2].y,
+                         m[0].z, m[1].z, m[2].z);
+}
+
+[[nodiscard]] constexpr auto transpose(const double4x4 m) noexcept {
+    return make_double4x4(m[0].x, m[1].x, m[2].x, m[3].x,
                          m[0].y, m[1].y, m[2].y, m[3].y,
                          m[0].z, m[1].z, m[2].z, m[3].z,
                          m[0].w, m[1].w, m[2].w, m[3].w);
@@ -460,6 +521,133 @@ template<size_t N>
     return dot0.x + dot0.y + dot0.z + dot0.w;
 }
 
+
+[[nodiscard]] constexpr auto inverse(const double2x2 m) noexcept {
+    const auto one_over_determinant = 1.0f / (m[0][0] * m[1][1] - m[1][0] * m[0][1]);
+    return make_double2x2(m[1][1] * one_over_determinant,
+                         -m[0][1] * one_over_determinant,
+                         -m[1][0] * one_over_determinant,
+                         +m[0][0] * one_over_determinant);
+}
+
+[[nodiscard]] constexpr auto inverse(const double3x3 m) noexcept {// from GLM
+    const auto one_over_determinant = 1.0f /
+                                      (m[0].x * (m[1].y * m[2].z - m[2].y * m[1].z) +
+                                       m[1].x * (m[2].y * m[0].z - m[0].y * m[2].z) +
+                                       m[2].x * (m[0].y * m[1].z - m[1].y * m[0].z));
+    return make_double3x3(
+        (m[1].y * m[2].z - m[2].y * m[1].z) * one_over_determinant,
+        (m[2].y * m[0].z - m[0].y * m[2].z) * one_over_determinant,
+        (m[0].y * m[1].z - m[1].y * m[0].z) * one_over_determinant,
+        (m[2].x * m[1].z - m[1].x * m[2].z) * one_over_determinant,
+        (m[0].x * m[2].z - m[2].x * m[0].z) * one_over_determinant,
+        (m[1].x * m[0].z - m[0].x * m[1].z) * one_over_determinant,
+        (m[1].x * m[2].y - m[2].x * m[1].y) * one_over_determinant,
+        (m[2].x * m[0].y - m[0].x * m[2].y) * one_over_determinant,
+        (m[0].x * m[1].y - m[1].x * m[0].y) * one_over_determinant);
+}
+
+[[nodiscard]] constexpr auto inverse(const double4x4 m) noexcept {// from GLM
+    const auto coef00 = m[2].z * m[3].w - m[3].z * m[2].w;
+    const auto coef02 = m[1].z * m[3].w - m[3].z * m[1].w;
+    const auto coef03 = m[1].z * m[2].w - m[2].z * m[1].w;
+    const auto coef04 = m[2].y * m[3].w - m[3].y * m[2].w;
+    const auto coef06 = m[1].y * m[3].w - m[3].y * m[1].w;
+    const auto coef07 = m[1].y * m[2].w - m[2].y * m[1].w;
+    const auto coef08 = m[2].y * m[3].z - m[3].y * m[2].z;
+    const auto coef10 = m[1].y * m[3].z - m[3].y * m[1].z;
+    const auto coef11 = m[1].y * m[2].z - m[2].y * m[1].z;
+    const auto coef12 = m[2].x * m[3].w - m[3].x * m[2].w;
+    const auto coef14 = m[1].x * m[3].w - m[3].x * m[1].w;
+    const auto coef15 = m[1].x * m[2].w - m[2].x * m[1].w;
+    const auto coef16 = m[2].x * m[3].z - m[3].x * m[2].z;
+    const auto coef18 = m[1].x * m[3].z - m[3].x * m[1].z;
+    const auto coef19 = m[1].x * m[2].z - m[2].x * m[1].z;
+    const auto coef20 = m[2].x * m[3].y - m[3].x * m[2].y;
+    const auto coef22 = m[1].x * m[3].y - m[3].x * m[1].y;
+    const auto coef23 = m[1].x * m[2].y - m[2].x * m[1].y;
+    const auto fac0 = double4{coef00, coef00, coef02, coef03};
+    const auto fac1 = double4{coef04, coef04, coef06, coef07};
+    const auto fac2 = double4{coef08, coef08, coef10, coef11};
+    const auto fac3 = double4{coef12, coef12, coef14, coef15};
+    const auto fac4 = double4{coef16, coef16, coef18, coef19};
+    const auto fac5 = double4{coef20, coef20, coef22, coef23};
+    const auto Vec0 = double4{m[1].x, m[0].x, m[0].x, m[0].x};
+    const auto Vec1 = double4{m[1].y, m[0].y, m[0].y, m[0].y};
+    const auto Vec2 = double4{m[1].z, m[0].z, m[0].z, m[0].z};
+    const auto Vec3 = double4{m[1].w, m[0].w, m[0].w, m[0].w};
+    const auto inv0 = Vec1 * fac0 - Vec2 * fac1 + Vec3 * fac2;
+    const auto inv1 = Vec0 * fac0 - Vec2 * fac3 + Vec3 * fac4;
+    const auto inv2 = Vec0 * fac1 - Vec1 * fac3 + Vec3 * fac5;
+    const auto inv3 = Vec0 * fac2 - Vec1 * fac4 + Vec2 * fac5;
+    constexpr auto sign_a = double4{+1.0f, -1.0f, +1.0f, -1.0f};
+    constexpr auto sign_b = double4{-1.0f, +1.0f, -1.0f, +1.0f};
+    const auto inv_0 = inv0 * sign_a;
+    const auto inv_1 = inv1 * sign_b;
+    const auto inv_2 = inv2 * sign_a;
+    const auto inv_3 = inv3 * sign_b;
+    const auto dot0 = m[0] * double4{inv_0.x, inv_1.x, inv_2.x, inv_3.x};
+    const auto dot1 = dot0.x + dot0.y + dot0.z + dot0.w;
+    const auto one_over_determinant = 1.0f / dot1;
+    return double4x4{inv_0 * one_over_determinant,
+                    inv_1 * one_over_determinant,
+                    inv_2 * one_over_determinant,
+                    inv_3 * one_over_determinant};
+}
+
+[[nodiscard]] constexpr auto determinant(const double2x2 m) noexcept {
+    return m[0][0] * m[1][1] - m[1][0] * m[0][1];
+}
+
+[[nodiscard]] constexpr auto determinant(const double3x3 m) noexcept {// from GLM
+    return m[0].x * (m[1].y * m[2].z - m[2].y * m[1].z) +
+           m[1].x * (m[2].y * m[0].z - m[0].y * m[2].z) +
+           m[2].x * (m[0].y * m[1].z - m[1].y * m[0].z);
+}
+
+[[nodiscard]] constexpr auto determinant(const double4x4 m) noexcept {// from GLM
+    const auto coef00 = m[2].z * m[3].w - m[3].z * m[2].w;
+    const auto coef02 = m[1].z * m[3].w - m[3].z * m[1].w;
+    const auto coef03 = m[1].z * m[2].w - m[2].z * m[1].w;
+    const auto coef04 = m[2].y * m[3].w - m[3].y * m[2].w;
+    const auto coef06 = m[1].y * m[3].w - m[3].y * m[1].w;
+    const auto coef07 = m[1].y * m[2].w - m[2].y * m[1].w;
+    const auto coef08 = m[2].y * m[3].z - m[3].y * m[2].z;
+    const auto coef10 = m[1].y * m[3].z - m[3].y * m[1].z;
+    const auto coef11 = m[1].y * m[2].z - m[2].y * m[1].z;
+    const auto coef12 = m[2].x * m[3].w - m[3].x * m[2].w;
+    const auto coef14 = m[1].x * m[3].w - m[3].x * m[1].w;
+    const auto coef15 = m[1].x * m[2].w - m[2].x * m[1].w;
+    const auto coef16 = m[2].x * m[3].z - m[3].x * m[2].z;
+    const auto coef18 = m[1].x * m[3].z - m[3].x * m[1].z;
+    const auto coef19 = m[1].x * m[2].z - m[2].x * m[1].z;
+    const auto coef20 = m[2].x * m[3].y - m[3].x * m[2].y;
+    const auto coef22 = m[1].x * m[3].y - m[3].x * m[1].y;
+    const auto coef23 = m[1].x * m[2].y - m[2].x * m[1].y;
+    const auto fac0 = double4{coef00, coef00, coef02, coef03};
+    const auto fac1 = double4{coef04, coef04, coef06, coef07};
+    const auto fac2 = double4{coef08, coef08, coef10, coef11};
+    const auto fac3 = double4{coef12, coef12, coef14, coef15};
+    const auto fac4 = double4{coef16, coef16, coef18, coef19};
+    const auto fac5 = double4{coef20, coef20, coef22, coef23};
+    const auto Vec0 = double4{m[1].x, m[0].x, m[0].x, m[0].x};
+    const auto Vec1 = double4{m[1].y, m[0].y, m[0].y, m[0].y};
+    const auto Vec2 = double4{m[1].z, m[0].z, m[0].z, m[0].z};
+    const auto Vec3 = double4{m[1].w, m[0].w, m[0].w, m[0].w};
+    const auto inv0 = Vec1 * fac0 - Vec2 * fac1 + Vec3 * fac2;
+    const auto inv1 = Vec0 * fac0 - Vec2 * fac3 + Vec3 * fac4;
+    const auto inv2 = Vec0 * fac1 - Vec1 * fac3 + Vec3 * fac5;
+    const auto inv3 = Vec0 * fac2 - Vec1 * fac4 + Vec2 * fac5;
+    constexpr auto sign_a = double4{+1.0f, -1.0f, +1.0f, -1.0f};
+    constexpr auto sign_b = double4{-1.0f, +1.0f, -1.0f, +1.0f};
+    const auto inv_0 = inv0 * sign_a;
+    const auto inv_1 = inv1 * sign_b;
+    const auto inv_2 = inv2 * sign_a;
+    const auto inv_3 = inv3 * sign_b;
+    const auto dot0 = m[0] * double4{inv_0.x, inv_1.x, inv_2.x, inv_3.x};
+    return dot0.x + dot0.y + dot0.z + dot0.w;
+}
+
 // transforms
 [[nodiscard]] constexpr auto translation(const float3 v) noexcept {
     return make_float4x4(
@@ -519,6 +707,67 @@ template<size_t N>
 [[nodiscard]] constexpr auto sign(float4 v) noexcept {
     return make_float4(sign(v.x), sign(v.y), sign(v.z), sign(v.w));
 }
+
+[[nodiscard]] constexpr auto translation(const double3 v) noexcept {
+    return make_double4x4(
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        v.x, v.y, v.z, 1.0f);
+}
+
+[[nodiscard]] constexpr auto translation(double x, double y, double z) noexcept {
+    return translation(make_double3(x, y, z));
+}
+
+
+[[nodiscard]] inline auto rotation(const double3 axis, double angle) noexcept {
+    if (angle == 0.0f) { return make_double4x4(1.0f); }
+    auto c = cos(angle);
+    auto s = sin(angle);
+    auto a = normalize(axis);
+    auto t = (1.0f - c) * a;
+    return make_double4x4(
+        c + t.x * a.x, t.x * a.y + s * a.z, t.x * a.z - s * a.y, 0.0f,
+        t.y * a.x - s * a.z, c + t.y * a.y, t.y * a.z + s * a.x, 0.0f,
+        t.z * a.x + s * a.y, t.z * a.y - s * a.x, c + t.z * a.z, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+[[nodiscard]] inline auto rotation(double ax, double ay, double az, double angle) noexcept {
+    return rotation(make_double3(ax, ay, az), angle);
+}
+
+[[nodiscard]] constexpr auto scaling(const double3 s) noexcept {
+    return make_double4x4(
+        s.x, 0.0f, 0.0f, 0.0f,
+        0.0f, s.y, 0.0f, 0.0f,
+        0.0f, 0.0f, s.z, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+[[nodiscard]] constexpr auto scaling(double x, double y, double z) noexcept {
+    return scaling(make_double3(x, y, z));
+}
+
+[[nodiscard]] constexpr auto scaling(double s) noexcept {
+    return scaling(make_double3(s));
+}
+
+[[nodiscard]] constexpr auto sign(double x) noexcept { return x < 0.f ? -1.f : 1.f; }
+
+[[nodiscard]] constexpr auto sign(double2 v) noexcept {
+    return make_double2(sign(v.x), sign(v.y));
+}
+
+[[nodiscard]] constexpr auto sign(double3 v) noexcept {
+    return make_double3(sign(v.x), sign(v.y), sign(v.z));
+}
+
+[[nodiscard]] constexpr auto sign(double4 v) noexcept {
+    return make_double4(sign(v.x), sign(v.y), sign(v.z), sign(v.w));
+}
+
 
 [[nodiscard]] constexpr auto sign(int x) noexcept { return x < 0 ? -1 : 1; }
 
